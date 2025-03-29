81bf733d376a028e43f570fc289f69c8412f9e44
unknown <athur4c@gmail.com>
2025-03-29 17:40:01 +0700

    mobile view

--- src/componenets/Contents/MobileOverlay.jsx
+++ src/componenets/Contents/MobileOverlay.jsx
@@ -1,3 +1,5 @@
+import ChatBubble from "../ChatBubbles/ChatBubble";
+import Footer from "../Footer/Footer";
 import "./mobileOverlay.css";
 
 const MobileOverlay = () => {
@@ -5,37 +7,67 @@ const MobileOverlay = () => {
     <>
       <section className="mobile_section">
         <div className="mobile_intro">
-          <h3 className="company_name">2G Education</h3>
-          <h1 className="main_name">GLOCAL</h1>
-          <h1 className="main_name">TREE</h1>
-          <p className="journey-quote">The journey 2G Education</p>
-          <p className="mobile_bounce ">↓</p>
+          <div className="main_name">
+            <h3 className="company_name">2G Group</h3>
+            <h1 className="connect">Connect</h1>
+            <h1 className="the">the</h1>
+            <h1 className="dots">"Đốt"</h1>
+          </div>
+        </div>
+
+        <div className="mobile-intro-second-group">
+          <div className="english">ANH NGỮ</div>
+          <div className="english-second">TOÀN DIỆN</div>
+          <div className="camping">TRẠI HÈ</div>
+          <div className="aboard">DU HỌC </div>
+        </div>
+
+        <div className="explaination">
+          <div className="company-explain">
+            <p>
+              {" "}
+              2G Group là một hệ thống giáo dục toàn diện, giúp thế hệ trẻ Việt
+              hội nhập toàn cầu thông qua việc đào tạo và du học.
+            </p>
+          </div>
+          <div className="theme-explain">
+            <p>
+              Với thông điệp Connect the “Đốt”, 2G Group mong muốn mở rộng sự
+              kết nối, góp phần phát triển một thế hệ giới trẻ mới, dám học, dám
+              làm, dám đi ra thế giới để thay đổi và cống hiến.
+            </p>
+          </div>
         </div>
       </section>
       <section className="mobile_section">
         <div className="mobile_feedback">
-          <h1>Customer </h1>
-          <h1> Feedback</h1>
-          <div className="mobile_video_container">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/cLNyF1Zw5tg?si=jJi9zINBoqG6QSHI"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
-            ></iframe>
+            <h1>Họ đang nói gì về 2G ? </h1>
+          <div className="chat-app">
+            <div className="chat-header">
+              <img
+                src="assets/images/favicon.png"
+                alt="Profile"
+                className="chat-avatar-large"
+              />
+              <p>2G Education</p>
+            </div>
+            <div className="chat-container">
+              <ChatBubble
+                message="Hello, What are they saying?"
+                isSender={false}
+              />
+              <ChatBubble message="No" isSender={true} />
+              <ChatBubble message="Not yet! What are you thinking?" isSender={false} />
+              <ChatBubble message="I'm thinking about it" isSender={true} />
+            </div>
           </div>
         </div>
-        <p className="mobile_bounce">↓</p>
       </section>
 
       <section className="mobile_section">
         <div className="mobile_achievement">
-          <h1>Our</h1>
-          <h1>Achievements</h1>
+          <h1>Thành tựu</h1>
+          <h1>2G Education</h1>
           <div className="mobile_timeline">
             <div className="mobile_timeline-item">
               <div className="mobile_content">
@@ -108,90 +140,11 @@ const MobileOverlay = () => {
       </section>
 
       <section className="mobile_section">
-        <div className="mobile_service">
-          <h1>Our </h1>
-          <h1> Service</h1>
-          <div className="mobile_product-container">
-            <div className="mobile_product-card">
-              <img
-                src="https://i.guim.co.uk/img/media/8c7f4fe66d305fb86fc3246dd47a9c06d216f7ec/0_139_1268_761/master/1268.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f27fa05d2f7629655beafeb9248c7647"
-                alt=""
-              />
-              <h4>2G Education</h4>
-              <h5>Học ngoại ngữ, mở tương lai</h5>
-              <p className="mobile_service-intro">
-                Giúp bạn chinh phục ngoại ngữ với chương trình đào tạo bài bản,
-                mở ra cơ hội học tập và sự nghiệp toàn cầu.{" "}
-              </p>
-              <p>- Đào tạo đa ngôn ngữ: Tiếng Anh, Tiếng Trung, Tiếng Đức.</p>
-              <p>- Luyện thi IELTS, THPT Chuyên với lộ trình cá nhân hoá.</p>
-              <p>
-                - Tiếng Anh giao tiếp & tiếng Anh trẻ em theo phương pháp tư duy
-                phản xạ.
-              </p>
-            </div>
-
-            <div className="mobile_product-card">
-              <img
-                src="https://i.guim.co.uk/img/media/8c7f4fe66d305fb86fc3246dd47a9c06d216f7ec/0_139_1268_761/master/1268.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f27fa05d2f7629655beafeb9248c7647"
-                alt=""
-              />
-              <h4>2G Pathways</h4>
-              <h5>Hành trình du học trọn gói từ A-Z</h5>
-              <p className="mobile_service-intro">
-                Dịch vụ tư vấn du học toàn diện, đồng hành từ khâu định hướng
-                đến khi đặt chân tới nước ngoài.
-              </p>
-              <p>- Tư vấn du học tại Anh, Mỹ, Úc, Canada, New Zealand.</p>
-              <p>- Xây dựng lộ trình cá nhân hoá, tối ưu hồ sơ và học bổng.</p>
-              <p>- Hỗ trợ toàn diện từ chuẩn bị giấy tờ đến phỏng vấn visa.</p>
-            </div>
-
-            <div className="mobile_product-card">
-              <img
-                src="https://i.guim.co.uk/img/media/8c7f4fe66d305fb86fc3246dd47a9c06d216f7ec/0_139_1268_761/master/1268.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f27fa05d2f7629655beafeb9248c7647"
-                alt=""
-              />
-              <h4>2G Eureka Camps</h4>
-              <h5>Cung cấp các giải pháp về mô hình trại hè</h5>
-              <p className="mobile_service-intro">
-                Trại hè đầu tiên tại Nghệ An mang đến hành trình khám phá văn
-                hoá và phát triển kỹ năng tại các quốc gia hàng đầu.
-              </p>
-              <p>
-                - Trại hè trao đổi tại Úc, New Zealand, Singapore dành cho trẻ
-                5-12 tuổi.
-              </p>
-              <p>
-                Chương trình kết hợp học tập, kỹ năng mềm & tư duy sáng tạo và
-                phản biện.{" "}
-              </p>
-              <p>
-                - Môi trường quốc tế giúp trẻ tự tin và phát triển toàn diện.
-              </p>
-            </div>
-
-            <div className="mobile_product-card">
-              <img
-                src="https://i.guim.co.uk/img/media/8c7f4fe66d305fb86fc3246dd47a9c06d216f7ec/0_139_1268_761/master/1268.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f27fa05d2f7629655beafeb9248c7647"
-                alt=""
-              />
-              <h4>GV Manpower</h4>
-              <h5>Cung cấp dịch vụ tư vấn du học nghề tại Đức</h5>
-              <p className="mobile_service-intro">
-                Chương trình du học nghề uy tín, giúp bạn xây dựng sự nghiệp ổn
-                định tại Đức với chi phí cực tối ưu.
-              </p>
-              <p>
-                - Tư vấn du học nghề tại Đức theo đúng ngành nghề tiềm năng.{" "}
-              </p>
-              <p>Đào tạo tiếng Đức bài bản, đạt chuẩn đầu vào.</p>
-              <p>Hỗ trợ toàn diện từ chuẩn bị hồ sơ đến khi sống tại Đức.</p>
-            </div>
-          </div>
-        </div>
+       
         <p className="mobile_bounce ">↓</p>
       </section>
+
+      <Footer />
     </>
   );
 };
