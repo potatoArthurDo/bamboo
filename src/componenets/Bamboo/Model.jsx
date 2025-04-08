50242ac3b59bc61015d6d8ddefff28b670a57d8b
unknown <athur4c@gmail.com>
2025-04-08 16:54:26 +0700

    new model and images

--- src/componenets/Bamboo/Model.jsx
+++ src/componenets/Bamboo/Model.jsx
@@ -14,7 +14,7 @@ import * as THREE from "three";
 
 gsap.registerPlugin(ScrollTrigger);
 export function Model(props) {
-  const { nodes, materials } = useGLTF("/assets/model/bamboo.glb");
+  const { nodes, materials } = useGLTF("/assets/model/bamboo_ver2.glb");
 
   //color
   const bambooMaterial = new MeshStandardMaterial({
@@ -90,10 +90,10 @@ export function Model(props) {
     //rotation
     tl.current.to(
       ref.current.rotation,
-      { duration: 1, x: 0, y: (60 * Math.PI) / 180, z: 0 },
+      { duration: 2, x: 0, y: -Math.PI / 32, z: 0 },
       0
     );
-    tl.current.to(ref.current.rotation, { duration: 1, x: 0, y: 0, z: 0 }, 9);
+    tl.current.to(ref.current.rotation, { duration: 1, x: 0, y: -Math.PI / 2, z: 0 }, 9);
 
     //movement
     tl.current.to(
@@ -101,8 +101,8 @@ export function Model(props) {
       {
         duration: 1,
         // x: -1,
-        // y:1.5,
-        x: 0.6,
+        // y:30,
+        x: -15,
         // y: 1,
         z: 3,
         ease: "power2.inOut",
@@ -114,7 +114,7 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        y: 1.5,
+        y: -6,
         ease: "power2.inOut",
         
       },
@@ -124,7 +124,7 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        y: 1,
+        y: -10,
         ease: "power2.inOut",
         
       },
@@ -134,7 +134,7 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        y: 0.8,
+        y: -16,
         ease: "power2.inOut",
         
       },
@@ -144,7 +144,7 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        y: 0.4,
+        y: -22,
         ease: "power2.inOut",
        
       },
@@ -154,7 +154,7 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        y: 0,
+        y: -24,
         ease: "power2.inOut",
         
       },
@@ -164,7 +164,7 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        y: -0.4,
+        y: -30,
         ease: "power2.inOut",
         
       },
@@ -174,7 +174,7 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        y: -0.8,
+        y: -36,
         ease: "power2.inOut",
         
       },
@@ -184,7 +184,7 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        y: -1,
+        y: -40,
         ease: "power2.inOut",
         
       },
@@ -194,9 +194,9 @@ export function Model(props) {
       ref.current.position,
       {
         duration: 1,
-        x: 0,
-        y: 0,
-        z: 0,
+        x: -20,
+        y: -20,
+        z: -50,
         ease: "power2.Out",
         // onUpdate: () => {
         //   console.log("step 9");
@@ -208,152 +208,233 @@ export function Model(props) {
   });
 
   return (
-    <group {...props} dispose={null} scale={[6.5, 6.5, 6.5]} ref={ref}>
-      <group scale={0.01}>
-        <group position={[-35.305, -35, 6.171]} scale={[2.213, 2.126, 2.399]}>
-          <group
-            position={[-1.15, 0, 1.571]}
-            rotation={[-Math.PI / 2, 0, 0]}
-            scale={[1.04, 1.201, 1]}
-          >
-            <group rotation={[Math.PI / 2, 0, 0]}>
-              <group position={[20.805, 0, 0]}>
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_15.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_16.geometry}
-                  material={leafMaterial}
-                />
-              </group>
-              <group position={[16.607, 0, 0]}>
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_34.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_35.geometry}
-                  material={leafMaterial}
-                />
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_36.geometry}
-                  material={leafMaterial}
-                />
-              </group>
-              <group position={[18.7, 1.222, 0]} rotation={[0, 0, -0.17]}>
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_38.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_39.geometry}
-                  material={leafMaterial}
-                />
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_40.geometry}
-                  material={leafMaterial}
-                />
-              </group>
-              <group
-                position={[15.205, 0.343, -0.185]}
-                rotation={[0, 0, 0.168]}
-                scale={[0.909, 1, 0.97]}
-              >
-                <mesh
-                  ref = {shoot1Ref}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_66001.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  ref = {shoot2Ref}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_66002.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  ref = {shoot3Ref}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_66003.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  ref = {shoot4Ref}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_66004.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  ref = {shoot5Ref}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_66005.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  ref = {shoot6Ref}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_66006.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  ref = {shoot7Ref}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_66007.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  ref = {shoot8Ref}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_66008.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_67.geometry}
-                  material={bambooMaterial}
-                />
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Object_68.geometry}
-                  material={leafMaterial}
-                />
-              </group>
-            </group>
-          </group>
-        </group>
-        <directionalLight
-          intensity={0.7}
-          decay={2}
-          rotation={[-0.506, 0.629, 0.756]}
+    <group {...props} dispose={null} ref={ref} position={[-18, -22, -50]} rotation={[0, -Math.PI / 2, 0]} >
+      <group position={[-0.162, 0, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_66.geometry}
+          material={materials.bambooMat}
+          position={[19.944, -0.109, -18.458]}
         />
-        <PerspectiveCamera
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_67.geometry}
+          material={materials.bamboobranch}
+          position={[19.944, -0.109, -18.458]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_68.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[19.944, -0.109, -18.458]}
+        />
+      </group>
+      <group position={[25.829, 0, -20]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_62.geometry}
+          material={materials.bamboobranch}
+          position={[-4.919, 1.302, 4.226]}
+          rotation={[-0.089, -0.055, -0.095]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_63.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[-4.919, 1.302, 4.226]}
+          rotation={[-0.089, -0.055, -0.095]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_64.geometry}
+          material={materials.bambooold}
+          position={[-4.919, 1.302, 4.226]}
+          rotation={[-0.089, -0.055, -0.095]}
+        />
+      </group>
+      <group position={[16.607, 0, -20]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_58.geometry}
+          material={materials.bambooalpha_basecolor}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_59.geometry}
+          material={materials.bamboobranch}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_60.geometry}
+          material={materials.bambooold}
+        />
+      </group>
+      <group position={[-0.162, 0, -20]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_54.geometry}
+          material={materials.bambooMat}
+          position={[16.54, 0.517, 4.722]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_55.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[16.54, 0.517, 4.722]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_56.geometry}
+          material={materials.bambooold}
+          position={[16.54, 0.517, 4.722]}
+        />
+      </group>
+      <group position={[25.829, 0, -40]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_50.geometry}
+          material={materials.bamboobranch}
+          position={[-5.512, -1.344, 11.591]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_51.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[-5.512, -1.344, 11.591]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_52.geometry}
+          material={materials.bamboooldnew}
+          position={[-5.512, -1.344, 11.591]}
+        />
+      </group>
+      <group position={[16.607, 0, -40]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_46.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[0.246, -2.148, 12.384]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_47.geometry}
+          material={materials.bamboobranch}
+          position={[0.246, -2.148, 12.384]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_48.geometry}
+          material={materials.bamboooldnew}
+          position={[0.246, -2.148, 12.384]}
+        />
+      </group>
+      <group position={[-0.162, 0, -40]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_42.geometry}
+          material={materials.bamboobranch}
+          position={[25.982, 0.896, 20.56]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_43.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[25.982, 0.896, 20.56]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_44.geometry}
+          material={materials.bamboooldnew}
+          position={[25.982, 0.896, 20.56]}
+        />
+      </group>
+      <group position={[25.829, 0, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_38.geometry}
+          material={materials.bambooMat}
+          position={[-3.393, 1.869, -10.279]}
+          rotation={[-0.014, -0.009, -0.015]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_39.geometry}
+          material={materials.bamboobranch}
+          position={[-3.393, 1.869, -10.279]}
+          rotation={[-0.014, -0.009, -0.015]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_40.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[-3.393, 1.869, -10.279]}
+          rotation={[-0.014, -0.009, -0.015]}
+        />
+      </group>
+      <group position={[16.607, 0, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_34.geometry}
+          material={materials.bambooMat}
+          position={[6.636, 0.813, -14.976]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_35.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[6.636, 0.813, -14.976]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_36.geometry}
+          material={materials.bamboobranch}
+          position={[6.636, 0.813, -14.976]}
+        />
+      </group>
+      <group position={[30.389, 0, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_15.geometry}
+          material={materials.bamboobranch}
+          position={[-6.356, 2.192, -8.501]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object_16.geometry}
+          material={materials.bambooalpha_basecolor}
+          position={[-6.356, 2.192, -8.501]}
+        />
+         <PerspectiveCamera
           ref={camera}
           makeDefault={false}
           far={100000} 
@@ -367,4 +448,4 @@ export function Model(props) {
   );
 }
 
-useGLTF.preload("/assets/model/bamboo.glb"); 
+useGLTF.preload("/assets/model/bamboo_ver2.glb"); 
