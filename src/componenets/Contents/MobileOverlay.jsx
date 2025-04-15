8df053f4ec29c8ea7631a7c505728c6c6ac12b5e
unknown <athur4c@gmail.com>
2025-04-15 15:56:56 +0700

    one more time yall

--- src/componenets/Contents/MobileOverlay.jsx
+++ src/componenets/Contents/MobileOverlay.jsx
@@ -30,7 +30,8 @@ const MobileOverlay = () => {
         scrollTrigger: {
           trigger: item,
           start: "top 90%", // Start animation when the element hits 80% of the viewport
-          toggleActions: "play none none none",
+          toggleActions: "play reverse play reverse",
+          markers: true,
         },
       });
     });
